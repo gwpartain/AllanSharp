41cc7413fe573d0b74c0ab68e59d510da421be09
gwpartain <gwpartain@gmail.com>
2023-10-28 21:42:18 -0500

    Renamed starting class. Stubbed out first calc method

---
+++ AllanSharp/AllanCalcService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AllanSharp
+{
+    public class AllanCalcService
+    {
+        public void ModifiedAllanDeviation(IList<double> observations, double measurementInterval)
+        {
+            //use the count of observations and the interval to determine the sweep range in time to determine the start and stop periods
+            var startSeconds = 0;
+            var endSeconds = observations.Count * measurementInterval;
+
+
+        }
+
+
+        /*
+         * Need an overlapping and a non-overlapping calculation of the allen variance. The allen deviation is the square root of either of those calculations
+         */
+    }
+}
--- AllanSharp/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace AllanSharp
-{
-    public class Class1
-    {
-
-    }
-}
